2bfd667418c0813887ec52dd25df3344a2ced346
DX4GREY <dxablack@gmail.com>
2025-06-15 02:45:23 +0000

    main: KeyboardInterupt bug fix
    Signed-off-by: DX4GREY <dxablack@gmail.com>

--- modules/main.py
+++ modules/main.py
@@ -1,18 +1,27 @@
 #!/bin/python
 
-# Copyright (c) Dx4 2023
+# Copyright (c) Dx4 2023-2025
 
 # The FaAng Toolkit programmed and developed by Dx4
 # The FaAng Toolkit was published by Dx4 under MIT Licence
 
 # Legal atau tidaknya tergantung kalian yang menggunakan
+#!/bin/python
+
+# Copyright (c) Dx4 2023
 
 from lib.app import main
 import sys, platform, os
 from colorama import Fore
+import threading, time
 
 if __name__ == '__main__':
 	try:
 		main()
 	except KeyboardInterrupt:
-		print(f"\n{Fore.RED}[*]{Fore.RESET} Exiting...")
+		print(f"\n{Fore.RED}[*]{Fore.RESET} Killing all processes...")
+		time.sleep(1)
+		print(f"{Fore.MAGENTA}[*]{Fore.RESET} Have a nice day Hackers!")
+		time.sleep(3)
+		os.system('cls' if os.name == 'nt' else 'clear')
+		os._exit(1)
